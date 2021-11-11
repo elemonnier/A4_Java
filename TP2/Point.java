b5d84420092ad4d7b984e61e77a2cd0a5f043212
Etienne Lemonnier <56196021+elemonnier@users.noreply.github.com>
2021-11-11 10:44:22 +0100

    Create Point.java

---
+++ TP2/Point.java
@@ -0,0 +1,35 @@
+public class Point implements Moveable {
+    private int x;
+    private int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public void moveTo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
